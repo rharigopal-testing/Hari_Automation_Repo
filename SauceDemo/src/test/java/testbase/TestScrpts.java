126bc30ec4e7018b3baaa0b13cca2bd308c30d95
HariGopal <rajampeta.hari@gmail.com>
2025-09-16 22:24:09 +0530

    Fixed the framework execution issue

--- SauceDemo/src/test/java/testbase/TestScrpts.java
+++ SauceDemo/src/test/java/testbase/TestScrpts.java
@@ -10,71 +10,60 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import pages.LoginPage;
 
-public class TestScrpts{
+public class TestScrpts {
 
 	WebDriver driver;
-	
-	@BeforeTest
-	public void setup(String browser) {
-		switch (browser.toLowerCase()) {
-		case "chrome":
-			driver = new ChromeDriver();
-			break;
-
-		case "edge":
-			driver = new EdgeDriver();
-			break;
-
-		case "firefox":
-			driver = new FirefoxDriver();
-			break;
-
-		default:
-			System.out.println("Entered browser name is wrong, please check!");
-			break;
-		}
+	@Parameters({"browser"})
+	@BeforeClass
+	public void setup(String br) {
+		switch (br.toLowerCase()) {
+		case "chrome": driver = new ChromeDriver();break;
+		case "edge": driver = new EdgeDriver(); break;
+		case "firefox":	driver = new FirefoxDriver(); break;
+		default: System.out.println("Entered browser name is wrong, please check!");
+		return; }
 	}
 
-	@Test(priority = 1)
+	@Parameters({"url"})
+	@Test(priority=1) 
 	public void testLaunchUrl(String url) {
-
 		driver.get(url);
-		// driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
-	}
+		}
 
-	@DataProvider(name = "loginData")
-    public Object[][] getData() {
-        return new Object[][] {
-            {"standard_user", "secret_sauce"},   // valid
-            };
-    }
-	
-	
-	@Test(priority = 2,dataProvider = "loginData")
+	@Test(priority=2, dataProvider="dp")
 	public void testLogin(String username, String password) {
 		LoginPage lp = new LoginPage(driver);
 		lp.enterUsername(username);
 		lp.enterPassword(password);
 		lp.clickLogin();
 		System.out.println("Test executed with -> " + username + " / " + password);
-		
-    }
-	
 
-	@AfterTest
+	}
+
+	@AfterClass
 	public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-}
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+	@DataProvider(name="dp")
+	Object[][] loginData() {
+		Object data[][] = { { "standard_user", "secret_sauce" } // valid
+		};
+		return data;
+	}
+
 }
